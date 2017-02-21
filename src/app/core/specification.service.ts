3cbb1aa4b97a1809a208eb4a013f4d06a570cb66
lmw <lmwtclmwtc@163.com>
2017-02-21 12:09:49 +0800

    add new specification list

--- src/app/core/specification.service.ts
+++ src/app/core/specification.service.ts
@@ -18,28 +18,118 @@ export class SpecificationService {
     let jsonArray = [
       {
         "id": 1,
-        "name": "200*2*5",
-        "volume": 0.002
+        "name": "6x12x200",
+        "volume": 0.0144
       },
       {
         "id": 2,
-        "name": "200*2*6",
-        "volume": 0.0024
+        "name": "6x10x200",
+        "volume": 0.012
       },
       {
         "id": 3,
-        "name": "200*2*8",
-        "volume": 0.0032
+        "name": "6x9x200",
+        "volume": 0.0108
       },
       {
         "id": 4,
-        "name": "200*2*10",
-        "volume": 0.004
+        "name": "6x8x200",
+        "volume": 0.0096
       },
       {
         "id": 5,
-        "name": "200*6*10",
-        "volume": 0.012
+        "name": "6x6x200",
+        "volume": 0.0072
+      },
+      {
+        "id": 6,
+        "name": "5x10x200",
+        "volume": 0.01
+      },
+      {
+        "id": 7,
+        "name": "5x8x200",
+        "volume": 0.008
+      },
+      {
+        "id": 8,
+        "name": "5x6x200",
+        "volume": 0.006
+      },
+      {
+        "id": 9,
+        "name": "5x4x200",
+        "volume": 0.004
+      },
+      {
+        "id": 10,
+        "name": "4x10x200",
+        "volume": 0.008
+      },
+      {
+        "id": 11,
+        "name": "4x8x200",
+        "volume": 0.0064
+      },
+      {
+        "id": 12,
+        "name": "4x6x200",
+        "volume": 0.0048
+      },
+      {
+        "id": 13,
+        "name": "4.5x6x200",
+        "volume": 0.0054
+      },
+      {
+        "id": 14,
+        "name": "4x4x200",
+        "volume": 0.0032
+      },
+      {
+        "id": 15,
+        "name": "3x4x200",
+        "volume": 0.0024
+      },
+      {
+        "id": 16,
+        "name": "2x3x200",
+        "volume": 0.0012
+      },
+      {
+        "id": 17,
+        "name": "2x12x200",
+        "volume": 0.00048
+      },
+      {
+        "id": 18,
+        "name": "2x10x200",
+        "volume": 0.004
+      },
+      {
+        "id": 19,
+        "name": "2x8x200",
+        "volume": 0.0032
+      },
+      {
+        "id": 20,
+        "name": "2x6x200",
+        "volume": 0.0024
+      },
+      {
+        "id": 21,
+        "name": "1.2x10x200",
+        "volume": 0.0024
+      },
+      {
+        "id": 22,
+        "name": "1.2x8x200",
+        "volume": 0.00192
+      },
+      {
+        "id": 23,
+        "name": "1.2x6x200",
+        "volume": 0.00144
       }
     ];
 
